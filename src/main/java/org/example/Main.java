69d3d36049a622433f6e9bc1bee4de0c14c0663b
Serhii Skurativskyi <serhii.skurativskyi@gmail.com>
2023-03-11 14:53:15 +0200

    Actions -2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Home work 10!");
+        System.out.println("Home work 10! Gradle actions added!");
     }
 }
